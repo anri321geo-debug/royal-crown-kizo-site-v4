b52c5543072268632fe239342fafd0067da9e4ff
anri321geo-debug <anri321geo@gmail.com>
2026-04-20 02:13:25 +0400

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 ﻿window.KIZO_CONFIG = {
   // Public frontend config
   supabaseUrl: 'https://fhzzgfufyfpcluurnjnj.supabase.co',
-  supabaseAnonKey: 'sb_publishable_v0QLOETC2ox2JZtckpWLNA_HeegKyeX',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoenpnZnVmeWZwY2x1dXJuam5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MTQ2NzcsImV4cCI6MjA5MjE5MDY3N30.lchFggXFf1aRo488pO90GnlDNHsykw5q6n-9rjX7WxM',
 
   mediaTable: 'media_items',
   messagesTable: 'contact_messages',
